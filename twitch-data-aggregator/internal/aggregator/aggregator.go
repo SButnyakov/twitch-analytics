60e79e28e719b1f8dd79f208542ce7c4423a9575
Sergey <140865416+SButnyakov@users.noreply.github.com>
2024-12-19 23:57:32 +0300

    paralleling aggregate execution

--- twitch-data-aggregator/internal/aggregator/aggregator.go
+++ twitch-data-aggregator/internal/aggregator/aggregator.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"sync"
 	"twitch-data-aggregator/config"
 	"twitch-data-aggregator/internal/db/clickhouse"
 
@@ -16,65 +17,81 @@ var (
 )
 
 func Aggregate(cfg *config.Config, conn driver.Conn, client *redis.Client) {
-	games, err := clickhouse.GetAllGames(conn)
-	if err != nil {
-		log.Printf("failed to get all games: %v\n", err)
-	} else {
-		m := make(map[string]interface{}, len(games))
-		for _, v := range games {
-			m[fmt.Sprintf("game:%s", v)] = true
-		}
-		if err := client.MSet(context.Background(), m).Err(); err != nil {
-			log.Printf("failed to save all games: %v\n", err)
-		}
-	}
-
-	streamers, err := clickhouse.GetAllStreamers(conn)
-	if err != nil {
-		log.Printf("failed to get all streamers: %v\n", err)
-	} else {
-		m := make(map[string]interface{}, len(streamers))
-		for _, v := range streamers {
-			m[fmt.Sprintf("streamer:%s", v)] = true
-		}
-		if err := client.MSet(context.Background(), m).Err(); err != nil {
-			log.Printf("failed to save all streamers: %v\n", err)
-		}
-	}
-
-	for _, days := range periods {
-		gamesAvgViewers, err := clickhouse.GetGameAvgViewerCountForNDays(conn, days)
+	var wg sync.WaitGroup
+	wg.Add(4)
+	go func() {
+		games, err := clickhouse.GetAllGames(conn)
 		if err != nil {
-			log.Printf("failed to get average games online for %d days: %v\n", days, err)
+			log.Printf("failed to get all games: %v\n", err)
+		} else {
+			m := make(map[string]interface{}, len(games))
+			for _, v := range games {
+				m[fmt.Sprintf("game:%s", v)] = true
+			}
+			if err := client.MSet(context.Background(), m).Err(); err != nil {
+				log.Printf("failed to save all games: %v\n", err)
+			}
 		}
+		wg.Done()
+	}()
 
-		m := make(map[string]interface{}, len(gamesAvgViewers))
-		for _, v := range gamesAvgViewers {
-			m[fmt.Sprintf("average_game_online:%d:%s", days, v.Game)] = v.ViewersCount
-		}
-
-		if err := client.MSet(context.Background(), m).Err(); err != nil {
-			log.Printf("failed to save average games online for %d days: %v\n", days, err)
-		}
-
-		log.Printf("average games online for %d days saved\n", days)
-	}
-
-	for _, days := range periods {
-		streamersAvgViewers, err := clickhouse.GetStreamerAvgViewerCountForNDays(conn, days)
+	go func() {
+		streamers, err := clickhouse.GetAllStreamers(conn)
 		if err != nil {
-			log.Printf("failed to get average streamers online for %d days: %v\n", days, err)
+			log.Printf("failed to get all streamers: %v\n", err)
+		} else {
+			m := make(map[string]interface{}, len(streamers))
+			for _, v := range streamers {
+				m[fmt.Sprintf("streamer:%s", v)] = true
+			}
+			if err := client.MSet(context.Background(), m).Err(); err != nil {
+				log.Printf("failed to save all streamers: %v\n", err)
+			}
 		}
+		wg.Done()
+	}()
 
-		m := make(map[string]interface{}, len(streamersAvgViewers))
-		for _, v := range streamersAvgViewers {
-			m[fmt.Sprintf("average_streamer_online:%d:%s", days, v.Game)] = v.ViewersCount
+	go func() {
+		for _, days := range periods {
+			gamesAvgViewers, err := clickhouse.GetGameAvgViewerCountForNDays(conn, days)
+			if err != nil {
+				log.Printf("failed to get average games online for %d days: %v\n", days, err)
+			}
+
+			m := make(map[string]interface{}, len(gamesAvgViewers))
+			for _, v := range gamesAvgViewers {
+				m[fmt.Sprintf("average_game_online:%d:%s", days, v.Game)] = v.ViewersCount
+			}
+
+			if err := client.MSet(context.Background(), m).Err(); err != nil {
+				log.Printf("failed to save average games online for %d days: %v\n", days, err)
+			}
+
+			log.Printf("average games online for %d days saved\n", days)
 		}
+		wg.Done()
+	}()
 
-		if err := client.MSet(context.Background(), m).Err(); err != nil {
-			log.Printf("failed to save average streamers online for %d days: %v\n", days, err)
+	go func() {
+		for _, days := range periods {
+			streamersAvgViewers, err := clickhouse.GetStreamerAvgViewerCountForNDays(conn, days)
+			if err != nil {
+				log.Printf("failed to get average streamers online for %d days: %v\n", days, err)
+			}
+
+			m := make(map[string]interface{}, len(streamersAvgViewers))
+			for _, v := range streamersAvgViewers {
+				m[fmt.Sprintf("average_streamer_online:%d:%s", days, v.Game)] = v.ViewersCount
+			}
+
+			if err := client.MSet(context.Background(), m).Err(); err != nil {
+				log.Printf("failed to save average streamers online for %d days: %v\n", days, err)
+			}
+
+			log.Printf("average streamers online for %d days saved\n", days)
 		}
+		wg.Done()
+	}()
 
-		log.Printf("average streamers online for %d days saved\n", days)
-	}
+	wg.Wait()
 }
